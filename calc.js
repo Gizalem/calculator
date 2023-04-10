30d13f33c7866a0a0ce26aeca6b97a5dcf519e50
Gizalem <gizalemderege@gmail.com>
2023-04-10 14:55:03 +0300

    i have added age calculator

--- calc.js
+++ calc.js
@@ -11,8 +11,8 @@ function insert(num) {
 
 function clearResult() {
   result.value = '';
-  
 }
+
 function backspace() {
   if (result.value === "Infinity") {
     result.value = "";
@@ -23,11 +23,19 @@ function backspace() {
   else {
     result.value = result.value.slice(0, -1);
   }
-}  
+}
 
-function calculate(){
+function calculate() {
   result.value = eval(result.value);
 }
 
-
-
+function calculateAge() {
+  const today = new Date();
+  const birthDate = new Date(result.value);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  result.value = age;
+}
